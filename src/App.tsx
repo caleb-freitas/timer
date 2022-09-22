8e0713669ad9c8424998176c900f23257fed6765
caleb-freitas <calebfreitas@tutanota.com>
2022-09-22 17:14:16 -0300

    feat: added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { ThemeProvider } from "styled-components"
-import { Optional } from "./@types/utils"
-import { ButtonContainer, ButtonContainerProps } from "./Button.styles"
-import { GlobalStyle } from "./styles/global"
-import { defaultTheme } from "./styles/themes/default"
+import { ThemeProvider } from 'styled-components'
+import { Optional } from './@types/utils'
+import { ButtonContainer, ButtonContainerProps } from './Button.styles'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
-export type ButtonProps = Optional<ButtonContainerProps, "variant">
+export type ButtonProps = Optional<ButtonContainerProps, 'variant'>
 
-export function App({ variant = "primary" }: ButtonProps) {
-
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ButtonContainer variant={variant}>Send</ButtonContainer>
       <ButtonContainer variant="danger">Send</ButtonContainer>
-      <ButtonContainer variant="secondary">Send</ButtonContainer>
+      <ButtonContainer variant="primary">Send</ButtonContainer>
       <ButtonContainer variant="success">Send</ButtonContainer>
+      <ButtonContainer variant="secondary">Send</ButtonContainer>
+      <ButtonContainer>Send</ButtonContainer>
 
       <GlobalStyle />
     </ThemeProvider>
--- src/Button.styles.ts
+++ src/Button.styles.ts
@@ -1,29 +1,17 @@
-import styled from "styled-components"
-
-export type ButtonColors = "primary" | "secondary" | "danger" | "success"
+import styled from 'styled-components'
+export type ButtonColors = 'primary' | 'secondary' | 'danger' | 'success'
 
 export type ButtonContainerProps = {
   variant: ButtonColors
 }
 
-export type ButtonVariants = {
-  [B in ButtonColors]: string
-}
-
-const buttonVariants: ButtonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  success: "green"
-}
-
-export const ButtonContainer = styled.button<ButtonContainerProps>`
+export const ButtonContainer = styled.button<Partial<ButtonContainerProps>>`
   width: 100px;
   height: 40px;
   border-radius: 4px;
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme["green-500"]};
-  color: ${props => props.theme["white"]};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `
