adeff9608e24465c0ae6af5d09822212fce63689
caleb-freitas <calebfreitas@tutanota.com>
2022-09-22 17:37:33 -0300

    feat: added header with default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Optional } from './@types/utils'
-import { ButtonContainer, ButtonContainerProps } from './Button.styles'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './components/Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-
-export type ButtonProps = Optional<ButtonContainerProps, 'variant'>
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <ButtonContainer variant="danger">Send</ButtonContainer>
-      <ButtonContainer variant="primary">Send</ButtonContainer>
-      <ButtonContainer variant="success">Send</ButtonContainer>
-      <ButtonContainer variant="secondary">Send</ButtonContainer>
-      <ButtonContainer>Send</ButtonContainer>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
