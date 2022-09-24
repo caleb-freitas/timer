676139b69f70e6799048e071130c97b64d091c1e
caleb-freitas <calebfreitas@tutanota.com>
2022-09-23 23:26:16 -0300

    feat: added Header component

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,11 +1,24 @@
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components'
+
 import { Header } from './Header'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
+
+const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
+  background: ${(props) => props.theme['gray-800']};
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+`
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,54 @@
+import styled from 'styled-components'
+import { Scroll, Timer } from 'phosphor-react'
+
+import logo from '../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <nav>
+        <NavLink end to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink end to="/history/">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
