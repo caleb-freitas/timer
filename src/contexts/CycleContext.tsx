87e2ff44660807274e1e4de6605492a32106681d
caleb-freitas <calebfreitas@tutanota.com>
2022-09-28 17:04:01 -0300

    fix: fixed reducer return

--- src/contexts/CycleContext.tsx
+++ src/contexts/CycleContext.tsx
@@ -31,6 +31,10 @@ export function CycleContextProvider({ children }: CycleContextProviderProps) {
       if (storedCycleStateJSON) {
         return JSON.parse(storedCycleStateJSON)
       }
+      return {
+        cycles: [],
+        activeCycleId: null,
+      }
     },
   )
 
